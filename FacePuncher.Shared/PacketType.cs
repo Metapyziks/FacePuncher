a8697ca5f8f51cedde1c74172d29fe560043decb
Metapyziks <metapyziks@gmail.com>
2014-05-14 22:19:44 +0100

    Commented PacketType.cs

--- FacePuncher.Shared/PacketType.cs
+++ FacePuncher.Shared/PacketType.cs
@@ -1,5 +1,9 @@
 ﻿namespace FacePuncher
 {
+    /// <summary>
+    /// Used to specify the nature of the following packet
+    /// during communication between the server and client.
+    /// </summary>
     public enum PacketType : byte
     {
         LevelState = 1,
