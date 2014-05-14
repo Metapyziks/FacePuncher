e6c3ebf1f15ee36bd60826137836f404f2cdacaa
Metapyziks <metapyziks@gmail.com>
2014-05-14 20:21:07 +0100

    Moved Rendering.cs

--- FacePuncher/Rendering.cs
+++ FacePuncher/Rendering.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using FacePuncher.Entities;
 using FacePuncher.Geometry;
 
-namespace FacePuncher.Graphics
+namespace FacePuncher
 {
     /// <summary>
     /// Structure containing information used when
