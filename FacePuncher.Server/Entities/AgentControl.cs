3f03af4461f6cce8ee202de7bce62129298a7d6c
Metapyziks <metapyziks@gmail.com>
2014-05-14 21:32:53 +0100

    Commented AgentControl.cs

--- FacePuncher.Server/Entities/AgentControl.cs
+++ FacePuncher.Server/Entities/AgentControl.cs
@@ -2,22 +2,44 @@
 
 namespace FacePuncher.Entities
 {
-    class AgentControl : Component
+    /// <summary>
+    /// Abstract component for entities that should move
+    /// of their own volition.
+    /// </summary>
+    abstract class AgentControl : Component
     {
         private ulong _nextMove;
 
+        /// <summary>
+        /// Gets or sets the number of game ticks between
+        /// movements for this entity.
+        /// </summary>
         [ScriptDefinable]
         public ulong MovePeriod { get; set; }
 
+        /// <summary>
+        /// Checks to see if the entity can move at the
+        /// specified time.
+        /// </summary>
+        /// <param name="time">Current game time.</param>
+        /// <returns>True if the entity can move, false
+        /// otherwise.</returns>
         protected bool CanMove(ulong time)
         {
             return time >= _nextMove;
         }
 
+        /// <summary>
+        /// Attempts to move the entity in the specified direction.
+        /// </summary>
+        /// <param name="dir">Direction to move.</param>
+        /// <param name="time">Current game time.</param>
         protected void Move(Direction dir, ulong time)
         {
+            // Check to see if we can move.
             if (MovePeriod == 0 || !CanMove(time) || !Entity.CanMove(dir)) return;
 
+            // Move and update the movement cooldown.
             Entity.Move(dir);
             _nextMove = time + MovePeriod;
         }
