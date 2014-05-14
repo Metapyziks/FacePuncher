2ca530b0b35bf46eaa86449827d538da244180a8
Metapyziks <metapyziks@gmail.com>
2014-05-14 20:38:12 +0100

    Commented Rendering.cs

--- FacePuncher/Rendering.cs
+++ FacePuncher/Rendering.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 using FacePuncher.Entities;
@@ -7,8 +8,8 @@ using FacePuncher.Geometry;
 namespace FacePuncher
 {
     /// <summary>
-    /// Structure containing information used when
-    /// drawing tiles and entities.
+    /// Structure containing information used when drawing 
+    /// tiles and entities.
     /// </summary>
     public struct DrawAttributes
     {
@@ -22,6 +23,11 @@ namespace FacePuncher
         /// </summary>
         public int Flash;
 
+        /// <summary>
+        /// Initializes a DrawAttributes structure.
+        /// </summary>
+        /// <param name="time">Current game time at the time of drawing.</param>
+        /// <param name="flash">Flash state for animating entities.</param>
         public DrawAttributes(ulong time, int flash)
         {
             Time = time;
@@ -40,6 +46,14 @@ namespace FacePuncher
         /// </summary>
         public const int EntityFlashPeriod = 6;
 
+        /// <summary>
+        /// A map of the 16 possible neighbouring tile solidity
+        /// states to the characters that should be used when
+        /// drawing walls.
+        /// 
+        /// TODO: Move to a definition file, allowing for different
+        ///       types of walls?
+        /// </summary>
         static readonly char[] _sWallTiles = new[] {
             '\u006f', '\u006f', '\u006f', '\u00c9',
             '\u006f', '\u00cd', '\u00bb', '\u00cb',
@@ -47,14 +61,26 @@ namespace FacePuncher
             '\u00bc', '\u00ca', '\u00b9', '\u00ce'
         };
 
+        /// <summary>
+        /// Renders the tiles deemed visible within the room corresponding
+        /// to this visibility mask to the specified location on the screen.
+        /// </summary>
+        /// <param name="vis">Visibility mask to draw from.</param>
+        /// <param name="rect">Clipping rectangle in level-space.</param>
+        /// <param name="screenPos">Position on the screen that the top-left
+        /// of the rectangle should be drawn to.</param>
+        /// <param name="attribs">Attributes to be used when drawing tiles
+        /// and entities.</param>
         public static void Draw(this RoomVisibility vis, Rectangle rect, Position screenPos, DrawAttributes attribs)
         {
             var subRect = vis.Room.Rect.Intersection(rect);
             var roomPos = vis.Room.Rect.TopLeft;
 
+            // Move the screen position and clipping rectangle to room-space.
             screenPos += roomPos - rect.TopLeft;
             rect = subRect - roomPos;
 
+            // Draw each visible tile that is within the clipping rectangle.
             foreach (var tile in vis.GetVisible(attribs.Time)) {
                 if (rect.Intersects(tile.RelativePosition)) {
                     tile.Draw(screenPos + tile.RelativePosition, attribs);
@@ -62,10 +88,20 @@ namespace FacePuncher
             }
         }
 
+        /// <summary>
+        /// Renders an individual tile and any entities it contains to the
+        /// specified location on the screen.
+        /// </summary>
+        /// <param name="tile">Tile to draw.</param>
+        /// <param name="screenPos">Position on the screen to draw the tile.</param>
+        /// <param name="attribs">Attributes to be used when drawing the tile
+        /// and any entities.</param>
         public static void Draw(this Tile tile, Position screenPos, DrawAttributes attribs)
         {
+            // Don't draw nothing.
             if (tile.State == TileState.Void) return;
 
+            // If the tile is a wall, find which character to use.
             if (tile.State == TileState.Wall) {
                 int adj = 
                     (tile.GetNeighbour(Direction.East).State  == TileState.Wall ? 1 : 0) |
@@ -77,20 +113,26 @@ namespace FacePuncher
                 return;
             }
 
+            // Get an array of all entities that may be drawn.
             var drawables = tile.Entities
                 .Where(x => x.HasComponent<Drawable>())
                 .Select(x => x.GetComponent<Drawable>())
                 .ToArray();
             
+            // If there's something to draw...
             if (drawables.Length > 0) {
+                // Find which layer is the upper-most.
                 var layer = (DrawableLayer) drawables.Max(x => x.Layer);
 
+                // Find all drawables that are on that layer.
                 drawables = drawables
                     .Where(x => x.Layer == layer)
                     .ToArray();
 
+                // Select the drawable to draw based on the flash state.
                 var drawable = drawables[(attribs.Flash / EntityFlashPeriod) % drawables.Length];
 
+                // Draw the entity.
                 Display.SetCell(screenPos,
                     drawable.GetSymbol(attribs),
                     drawable.GetForeColor(attribs),
@@ -99,6 +141,9 @@ namespace FacePuncher
                 return;
             }
 
+            // When no drawable entities are present just draw the floor.
+            // TODO: Allow multiple types of floor with different symbols,
+            //       definable in definition files.
             Display.SetCell(screenPos, '+', ConsoleColor.DarkGray);
         }
     }
