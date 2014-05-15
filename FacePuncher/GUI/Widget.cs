f47d3c3703e0009a4642fe3f74456837057eecfa
Deseteral <deseteral@gmail.com>
2014-05-15 21:36:21 +0200

    Added basic Widget class

---
+++ FacePuncher/GUI/Widget.cs
@@ -0,0 +1,31 @@
+﻿using FacePuncher.Geometry;
+
+namespace FacePuncher.GUI
+{
+    abstract class Widget
+    {
+        protected Rectangle rectangle;
+
+        public Position Position
+        {
+            get { return rectangle.TopLeft; }
+        }
+
+        public int Width
+        {
+            get { return rectangle.Width; }
+        }
+
+        public int Height
+        {
+            get { return rectangle.Height; }
+        }
+
+        public Widget(Position pos, int width, int height)
+        {
+            this.rectangle = new Rectangle(pos, new Position(pos.X + width, pos.Y + height));
+        }
+
+        abstract public void Draw();
+    }
+}
