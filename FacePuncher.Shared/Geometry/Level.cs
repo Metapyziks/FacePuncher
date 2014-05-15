24a70fb9f67f3d5efe78a2f5216c1bb5c786703d
Metapyziks <metapyziks@gmail.com>
2014-05-15 20:58:35 +0100

    Commented Level.cs

--- FacePuncher.Shared/Geometry/Level.cs
+++ FacePuncher.Shared/Geometry/Level.cs
@@ -10,6 +10,9 @@ namespace FacePuncher.Geometry
     {
         private List<Room> _rooms;
 
+        /// <summary>
+        /// Gets or sets the current game time in steps.
+        /// </summary>
         public ulong Time { get; set; }
 
         /// <summary>
@@ -38,19 +41,32 @@ namespace FacePuncher.Geometry
             return room;
         }
 
+        /// <summary>
+        /// Gets the set of rooms that intersect a given rectangle.
+        /// </summary>
+        /// <param name="rect">Rectangle to test for intersections.</param>
+        /// <returns>A set of rooms that intersect with the given rectangle.</returns>
         public IEnumerable<Room> GetIntersectingRooms(Rectangle rect)
         {
             return _rooms.Where(x => x.Rect.Intersects(rect));
         }
 
+        /// <summary>
+        /// Gets a tile at the specified location in the level.
+        /// </summary>
+        /// <param name="x">Horizontal position of the tile.</param>
+        /// <param name="y">Vertical position of the tile.</param>
+        /// <returns>A tile from the specified location.</returns>
         public Tile this[int x, int y]
         {
-            get
-            {
-                return this[new Position(x, y)];
-            }
+            get { return this[new Position(x, y)]; }
         }
 
+        /// <summary>
+        /// Gets a tile at the specified location in the level.
+        /// </summary>
+        /// <param name="pos">Position of the tile to get.</param>
+        /// <returns>A tile from the specified location.</returns>
         public Tile this[Position pos]
         {
             get
@@ -62,20 +78,29 @@ namespace FacePuncher.Geometry
             }
         }
 
+        /// <summary>
+        /// Perform a single think step and increment the time counter.
+        /// </summary>
         public void Think()
         {
-            foreach (var room in _rooms) {
-                room.Think();
-            }
+            foreach (var room in _rooms) room.Think();
 
             ++Time;
         }
 
+        /// <summary>
+        /// Gets an enumerator that iterates through each room in the level.
+        /// </summary>
+        /// <returns>An enumerator that iterates through each room in the level.</returns>
         public IEnumerator<Room> GetEnumerator()
         {
             return _rooms.GetEnumerator();
         }
 
+        /// <summary>
+        /// Gets an enumerator that iterates through each room in the level.
+        /// </summary>
+        /// <returns>An enumerator that iterates through each room in the level.</returns>
         System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
         {
             return _rooms.GetEnumerator();
