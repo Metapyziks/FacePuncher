6833468136bf543f3008dbf75eb3e79d61c6d68d
Deseteral <deseteral@gmail.com>
2014-05-16 19:11:16 +0200

    Added GUI Manager class

---
+++ FacePuncher/GUI/GuiManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FacePuncher.GUI
+{
+    class GuiManager
+    {
+        public Dictionary<string, Widget> Widgets { get; set; }
+
+        public GuiManager()
+        {
+            Widgets = new Dictionary<string, Widget>();
+        }
+
+        public void AddWidget(string name, Widget w)
+        {
+            Widgets.Add(name, w);
+        }
+
+        public void Draw()
+        {
+            foreach (var widget in Widgets)
+            {
+                widget.Value.Draw();
+            }
+        }
+    }
+}
