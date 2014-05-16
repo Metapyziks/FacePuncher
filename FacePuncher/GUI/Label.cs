b384760ab61a4af779e50c65a94847f23dd8b113
Deseteral <deseteral@gmail.com>
2014-05-16 18:18:51 +0200

    Added Label widget

---
+++ FacePuncher/GUI/Label.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using FacePuncher.Geometry;
+
+namespace FacePuncher.GUI
+{
+    class Label : Widget
+    {
+        public string Text { get; set; }
+
+        public Label(Position pos, string text)
+            : base(pos, 0, 0)
+        {
+            this.Text = text;
+        }
+
+        public Label(Position pos, string text, ConsoleColor fc, ConsoleColor bc)
+            : base(pos, 0, 0, fc, bc)
+        {
+            this.Text = text;
+        }
+
+        public override void Draw()
+        {
+            for (var x = 0; x < Text.Length; x++)
+            {
+                Display.SetCell(Position.X + x, Position.Y, Text[x], ForegroundColor, BackgroundColor);
+            }
+        }
+    }
+}
