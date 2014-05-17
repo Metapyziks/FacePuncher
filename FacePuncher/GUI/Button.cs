7469591ca359e9c5c8e3c6461aa6a9af72426d7c
Deseteral <deseteral@gmail.com>
2014-05-17 15:06:54 +0200

    Added basic Button class

---
+++ FacePuncher/GUI/Button.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using FacePuncher.Geometry;
+
+namespace FacePuncher.GUI
+{
+    class Button : Widget
+    {
+        public string Text { get; set; }
+
+        public Button(Position pos, string text,
+            ConsoleColor fc = ConsoleColor.Gray, ConsoleColor bc = ConsoleColor.Black)
+            : base(pos, 0, 0, true, fc, bc)
+        {
+            this.Text = text;
+        }
+
+        public override void Draw()
+        {
+            ConsoleColor fc = ForegroundColor;
+            ConsoleColor bc = BackgroundColor;
+
+            // If widget is selected
+            // swap colors
+            if (IsSelectable && IsSelected)
+            {
+                fc = BackgroundColor;
+                bc = ForegroundColor;
+            }
+
+            GuiManager.DrawString(Position, Text, fc, bc);
+        }
+    }
+}
