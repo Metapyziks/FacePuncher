b3b69368f5fca9f4eb67e76ecae77353880eb63f
Deseteral <deseteral@gmail.com>
2014-05-17 13:07:10 +0200

    Created Widget container interface

---
+++ FacePuncher/GUI/IWidgetContainer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FacePuncher.GUI
+{
+    interface IWidgetContainer
+    {
+        Dictionary<string, Widget> Children { get; set; }
+
+        void AddChild(string name, Widget w);
+        void DrawChildren();
+    }
+}
