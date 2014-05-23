bc3b4093bbb2ebfec0340f836d56f53dbae6862c
Metapyziks <metapyziks@gmail.com>
2014-05-23 17:02:45 +0100

    Updated the license to GPL v3

--- FacePuncher.Shared/Intents.cs
+++ FacePuncher.Shared/Intents.cs
@@ -1,19 +1,19 @@
-﻿/* Copyright (C) 2014 Tamme Schichler (tammeschichler@googlemail.com)
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
+/* Copyright (c) 2014 Tamme Schichler [tammeschichler@googlemail.com]
+ * 
+ * This file is part of FacePuncher.
+ * 
+ * FacePuncher is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * FacePuncher is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
- * USA
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with FacePuncher. If not, see <http://www.gnu.org/licenses/>.
  */
 
 using FacePuncher.Geometry;
--- FacePuncher/UI/ServerSelectPrompt.cs
+++ FacePuncher/UI/ServerSelectPrompt.cs
@@ -1,21 +1,21 @@
-﻿/* Copyright (C) 2014 James King (metapyziks@gmail.com)
- * Copyright (C) 2014 Saša Barišić (cartman300@net.hr)
- * Copyright (C) 2014 Michał Ferchow (deseteral@gmail.com)
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
+/* Copyright (c) 2014 James King [metapyziks@gmail.com]
+ * Copyright (c) 2014 Saša Barišić [cartman300@net.hr]
+ * Copyright (c) 2014 Michał Ferchow [deseteral@gmail.com]
+ * 
+ * This file is part of FacePuncher.
+ * 
+ * FacePuncher is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * FacePuncher is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
- * USA
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with FacePuncher. If not, see <http://www.gnu.org/licenses/>.
  */
 
 using System;
